58bfe6cf1aab7a7a93209de8919b8e34cd4aed20
Kyle Rimando <riman004@csusm.edu>
2019-07-24 15:56:15 -0700

    Colorize Buttons

---
+++ WeLearn/M2-javaScript/Labs/script.js
@@ -0,0 +1,50 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Use querySelector to store the div in a variable.
+const redButton = document.querySelector('#red');
+const greenButton = document.querySelector('#green');
+const blueButton = document.querySelector('#blue');
+const theBox = document.querySelector('#response-box')
+let clear = document.querySelector('#clear');
+let list = '';
+// Use addEventListener to respond to a click event.
+
+redButton.addEventListener('click', (e) => {
+    console.log("You clicked the red button!");
+  list=list + "red "
+  theBox.innerHTML = list;
+  theBox.style.backgroundColor = 'red';
+});
+
+greenButton.addEventListener('click', (e) => {
+    console.log("You clicked the green button!");
+  list=list + "green "
+  theBox.innerHTML = list;
+  theBox.style.backgroundColor = 'green';
+});
+
+blueButton.addEventListener('click', (e) => {
+    console.log("You clicked the blue button!");
+  list=list + "blue "
+  theBox.innerHTML = list;
+  theBox.style.backgroundColor = 'blue';
+});
+
+clear.addEventListener('click', (e) => {
+  console.log("You clicked the clear buttom!");
+  list = '';
+  theBox.style.backgroundColor = 'white';
+  theBox.innerHTML = list;
+});
