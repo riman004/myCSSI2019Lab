7baa33f0c9e3319340500a64db09c1f332367f6a
Kyle Rimando <riman004@csusm.edu>
2019-07-25 09:36:23 -0700

    Learning how to Pair

---
+++ WeLearn/M2-javaScript/L3-JSObject-Array/console/script.js
@@ -0,0 +1,9 @@
+const article = {
+  name: 'Man Runs Around Melrose Naked',
+  views: 100000000,
+  datePublished: '03/25/2069',
+  author: {
+    name: 'Tyler Haley',
+    title: 'Fat Idiots and Rolling Dough'
+  }
+};
